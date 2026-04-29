3ba9f308a5a0844845fc751c7effc90ba588357f
carolbalzani <carolcbalzani@gmail.com>
2026-04-28 22:16:29 -0300

    Modificação

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title>Login</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <form method="post" action="login.php">
+      <label>CPF</label> 
+  <input type="text" name="cpf"><br>
+      <label>Senha:</label>
+      <input type="password" name="senha"><br>
+      <input type="submit" value="Entrar">
+    </form>
+  </body>
+</html>
